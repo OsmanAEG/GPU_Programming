4bff55758ef2ff05ae5ce98d6ab814680b7bc4ea
Osman El-Ghotmi <79581083+OsmanAEG@users.noreply.github.com>
2025-01-24 23:09:48 -0500

    added matmul usm

--- sycl_examples/sycl_tools/usm.h
+++ sycl_examples/sycl_tools/usm.h
@@ -2,7 +2,7 @@
 #define USM_H
 
 ///////////////////////////////////////////////////////////////////////
-// This file is part of the Osman's GPU_Programming github repo
+// This file is part of Osman's GPU_Programming github repo
 // It is licensed under the MIT licence.  A copy of
 // this license, in a file named LICENSE.md, should have been
 // distributed with this file.  A copy of this license is also
@@ -22,6 +22,21 @@
 ///////////////////////////////////////////////////////////////////////
 #include <sycl/sycl.hpp>
 
-namespace sycl_tools {} // namespace sycl_tools
+namespace sycl_tools {
+
+// reserving usm memory
+template<typename Queue_T, typename Vector_T>
+auto make_usm(Queue_T& Q, Vector_T& vec) {
+  using Scalar_T = typename Vector_T::value_type;
+  const size_t N = vec.size();
+
+  Scalar_T* usm_mem = sycl::malloc_shared<Scalar_T>(N, Q);
+
+  Q.memcpy(usm_mem, vec.data(), N*sizeof(Scalar_T)).wait();
+
+  return usm_mem;
+}
+
+} // namespace sycl_tools
 
 #endif // USM_H
